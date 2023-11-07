a7381fccb85b4be74d8a64cd365452bf4d102f28
khoi.nguyen <knguyenanh8194@gmail.com>
2023-11-07 18:03:48 +0700

    Checking datalayer

--- data-stream/config_env.sh
+++ data-stream/config_env.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-export GCP_PROJECT="<project-id>"
+export GCP_PROJECT="khoi-test-001-01"
 export ENDPOINT_URL="<endpoint-url>" # doesn't need to be defined in the very beginning
 export PUSH_ENDPOINT='<processing-endpoint-url>' # doesn't need to be defined in the very beginning
-export GCP_REGION=europe-west1
+export GCP_REGION=us-central1
 export RUN_PROXY_DIR=cloud-run-pubsub-proxy
 export RUN_PROCESSING_DIR=processing-service
 export DATAFLOW_TEMPLATE=beam
